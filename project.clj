89480951a62049c518f8bb62dcaf886dd60ccc37
Vinicius Gravina da Rocha <vinicius.darocha@gmail.com>
2015-09-17 08:47:45 -0600

    Adapted project.clj for clojars release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject hiccup-table "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject hiccup-table "0.1.0"
+  :description "A library to easily generate html tables from clojure data structures with the possibility of defining the tags atributes as functions/maps."
+  :url "https://github.com/vgrocha/hiccup-table"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
